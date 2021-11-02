43894a8ed9d98755556d6354b41a188f2dbdb86b
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-11-02 18:15:37 -0400

    update

--- timer1.js
+++ timer1.js
@@ -1,21 +1,40 @@
 
 //console.log(Number(inp));
 
-const timer = function(inp){
+//const timer = function(inp){
 
-  if(Number(inp) === "Nan" || Number(inp) <= 0 || inp === ""){
-    return exit;
-  } else {
-    setTimeout(() => {
-      process.stdout.write('\007');
-    }, inp);
+//   if(!Number(inp) === "Nan" || Number(inp) <= 0 || inp === ""){
+//     return "exit";
+//   } else {
+//     setTimeout(() => {
+//       process.stdout.write('\007');
+//     }, inp);
 
-  }
+//   }
+// }
+
+
+// const inp = process.argv.slice(2);
+// console.log(timer(inp));
+// //process.stdout.write('\x07');
+// //console.log('\u0007');
+
+const beep = function() {
+  console.log('beep');
+
 }
 
+const scheduleBeep = function(delay) {
+  delay = Number(delay);
 
-const inp = process.argv.slice(2);
-console.log(timer(inp));
-//process.stdout.write('\u0007');
-//console.log('\u0007');
+  if(!isNaN(delay) && delay > 0 ) {
+    setTimeout(beep, delay * 1000);
+  }
 
+}
+
+const number = process.argv.slice(2);
+
+for(const num of number) {
+  scheduleBeep(num);
+}
